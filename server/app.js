4a768a07b11123b4ba5ce3f0671f56d575463609
Abhishekkv <abhishekkvpnld@gmail.com>
2024-08-21 15:41:52 +0530

    initial setup

---
+++ server/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import dbConnection from "./config/DbConnection.js";
+import userRoute from "./routes/user_route.js";
+import postRoute from "./routes/post_route.js";
+
+dotenv.config();
+
+const app = express();
+
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(
+{
+    origin:[
+        process.env.FRONTEND_URL,
+        "http://localhost:5174",
+    ]
+}
+));
+
+//Routes
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/post",postRoute);
+
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Server Running...");
+});
+
+dbConnection().then(()=>{
+    app.listen(PORT, () => {
+        console.log(`Server Running On Port ${PORT} 🚀🚀`);
+      });
+});
---
+++ server/config/DbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const dbConnection =  async ()=>{
+
+try {
+   await mongoose.connect(process.env.MONGO_LOCAL_URI,{});        
+   console.log("Server connected to database successfully...💻✅");   
+} catch (error) {
+    console.log(error);
+};
+
+};
+
+export default dbConnection;
